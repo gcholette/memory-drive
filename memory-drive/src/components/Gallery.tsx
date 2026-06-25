af21b453c95cd0283959b09b2881f694e1e6db7e
gcholette <solutions@gcholette.com>
2026-06-24 20:10:02 -0400

    feat: wip on the UI

---
+++ memory-drive/src/components/Gallery.tsx
@@ -0,0 +1,42 @@
+import { useArchiveStore } from "../store/archiveStore";
+import { useSelectionStore } from "../store/selectionStore";
+import "./Gallery.css";
+import { GalleryItem } from "./GalleryItem";
+
+export const Gallery = () => {
+    const { selectedYear, setSelectedYear } = useSelectionStore()
+    const { archiveMetadata } = useArchiveStore()
+
+    if (selectedYear === null) return null
+    if (archiveMetadata === null) return null
+
+    const { total_imgs, total_vids, year_months } = archiveMetadata?.years[selectedYear]
+    const months = Object.keys(year_months)
+
+    return (
+        <div className="window">
+            <div className="title-bar">
+                <div className="title-bar-text">{selectedYear}</div>
+                <div className="title-bar-controls">
+                    <button aria-label="Minimize"></button>
+                    <button aria-label="Maximize"></button>
+                    <button aria-label="Close" onClick={() => setSelectedYear(null)}></button>
+                </div>
+            </div>
+
+            <div className="window-body">
+                {months.map(m => <div className="gallery-month">
+                    <h4>{m}</h4>
+                    {year_months[Number(m)].imgs.map(img => <GalleryItem ressource={img} />)}
+
+                </div>)}
+
+            </div>
+            <div className="status-bar">
+                <p className="status-bar-field">{total_imgs} pictures</p>
+                <p className="status-bar-field">{total_vids} videos</p>
+                <p className="status-bar-field">{months.length} months</p>
+            </div>
+        </div>
+    )
+}
--- memory-drive/src/components/Sidebar.tsx
+++ memory-drive/src/components/Sidebar.tsx
@@ -1,17 +1,34 @@
+import { invoke } from "@tauri-apps/api/core";
 import { useArchiveStore } from "../store/archiveStore"
 import "./Sidebar.css";
+import { ArchiveMetadata } from "../types/archiveMetadata";
+import { useSelectionStore } from "../store/selectionStore";
 
 export const Sidebar = () => {
-    const { archiveMetadata } = useArchiveStore()
+    const { archiveMetadata, setArchiveMetadata } = useArchiveStore()
+    const { setSelectedYear } = useSelectionStore()
+
+    const load_archive = async () => {
+        const archiveData = (await invoke("load_archive", { archivePath: '/home/gcholette/Pictures/mdrive_test/' })) as ArchiveMetadata
+
+        setArchiveMetadata(archiveData)
+        console.log(archiveData)
+    }
+
 
     const years = Object.keys(archiveMetadata?.years || {})
 
-    return <div className="sidebar">
-        {years.map(yr => <div>
-            <button key={yr}>
-                {yr}
-            </button>
+    return (
+        <div className="sidebar">
+            {years.map(yr => <div>
+                <button key={yr} className="year" onClick={() => setSelectedYear(Number(yr))}>
+                    {yr.split('').map(x => `${x} `).join('')}
+                </button>
+            </div>
+            )}
+            <div>
+                <button type="submit" onClick={load_archive}>Load Archive</button>
+            </div>
         </div>
-        )}
-    </div>
+    )
 }
